2ba766e81466105a70fbeacfaae8adc38dc5a13e
Mangesh Hambarde <mhambarde1@sheffield.ac.uk>
2019-09-03 03:46:43 +0100

    Trying to add augmentation to v4 (vox1dev xvec)

--- egs/dihard2-voxceleb/v4/run.sh
+++ egs/dihard2-voxceleb/v4/run.sh
@@ -24,6 +24,9 @@ DIHARD_EVAL_DIR=/share/mini5/data/audvis/dia/dihard-2018-eval-for-use-with-2019-
 # Path to Voxceleb I.
 VOXCELEB_DIR=/share/mini5/data/audvis/dia/voxceleb/voxceleb1
 
+musan_root=/share/mini5/data/audvis/dia/musan
+RIR_PATH=/share/mini5/data/audvis/dia/RIRS_NOISES
+
 nnet_dir=exp/xvector_nnet_1a
 
 dihard_dev=dihard_dev_2018
@@ -83,17 +86,77 @@ if [ $stage -le 1 ]; then
 fi
 echo "Stage 1: Extract MFCC and compute VAD done."
 
+# In this section, we augment the VoxCeleb2 data with reverberation,
+# noise, music, and babble, and combine it with the clean data.
+if [ $stage -le 2 ]; then
+  # Make a version with reverberated speech
+  rvb_opts=()
+  rvb_opts+=(--rir-set-parameters "0.5, $RIR_PATH/simulated_rirs/smallroom/rir_list")
+  rvb_opts+=(--rir-set-parameters "0.5, $RIR_PATH/simulated_rirs/mediumroom/rir_list")
+
+  # Make a reverberated version of the VoxCeleb2 list.  Note that we don't add any
+  # additive noise here.
+  steps/data/reverberate_data_dir.py \
+    "${rvb_opts[@]}" \
+    --speech-rvb-probability 1 \
+    --pointsource-noise-addition-probability 0 \
+    --isotropic-noise-addition-probability 0 \
+    --num-replications 1 \
+    --source-sampling-rate 16000 \
+    data/train_with_sil data/train_reverb
+  cp data/train_with_sil/vad.scp data/train_reverb/
+  utils/copy_data_dir.sh --utt-suffix "-reverb" data/train_reverb data/train_reverb.new
+  rm -rf data/train_reverb
+  mv data/train_reverb.new data/train_reverb
+
+  # Prepare the MUSAN corpus, which consists of music, speech, and noise
+  # suitable for augmentation.
+  steps/data/make_musan.sh --sampling-rate 16000 $musan_root data
+
+  # Get the duration of the MUSAN recordings.  This will be used by the
+  # script augment_data_dir.py.
+  for name in speech noise music; do
+    utils/data/get_utt2dur.sh data/musan_${name}
+    mv data/musan_${name}/utt2dur data/musan_${name}/reco2dur
+  done
+
+  # Augment with musan_noise
+  steps/data/augment_data_dir.py --utt-suffix "noise" --fg-interval 1 --fg-snrs "15:10:5:0" --fg-noise-dir "data/musan_noise" data/train_with_sil data/train_noise
+  # Augment with musan_music
+  steps/data/augment_data_dir.py --utt-suffix "music" --bg-snrs "15:10:8:5" --num-bg-noises "1" --bg-noise-dir "data/musan_music" data/train_with_sil data/train_music
+  # Augment with musan_speech
+  steps/data/augment_data_dir.py --utt-suffix "babble" --bg-snrs "20:17:15:13" --num-bg-noises "3:4:5:6:7" --bg-noise-dir "data/musan_speech" data/train_with_sil data/train_babble
+
+  # Combine reverb, noise, music, and babble into one directory.
+  utils/combine_data.sh data/train_aug data/train_reverb data/train_noise data/train_music data/train_babble
+fi
+echo "Stage 2: Combine reverb, noise, music, babble done."
+
+if [ $stage -le 3 ]; then
+  # Make MFCCs for the augmented data.  Note that we do not compute a new
+  # vad.scp file here.  Instead, we use the vad.scp from the clean version of
+  # the list.
+  steps/make_mfcc.sh --mfcc-config conf/mfcc.conf --nj 40 --cmd "$train_cmd" \
+    data/train_aug exp/make_mfcc $mfccdir
+
+  # Combine the clean and augmented VoxCeleb2 list.  This is now roughly
+  # double the size of the original clean list.
+  utils/combine_data.sh data/train data/train_aug data/train_with_sil
+fi
+echo "Stage 3: Combine augment and non-augment done."
+
 # Now we prepare the features to generate examples for xvector training.
 if [ $stage -le 4 ]; then
   # This script applies CMVN and removes nonspeech frames.  Note that this is somewhat
   # wasteful, as it roughly doubles the amount of training data on disk.  After
   # creating training examples, this can be removed.
   local/nnet3/xvector/prepare_feats_for_egs.sh --nj 40 --cmd "$train_cmd" \
-    data/train_with_sil data/train exp/train
-  cp data/train_with_sil/segments data/train
-  cp data/train_with_sil/vad.scp data/train
+    data/train data/train exp/train
+  cp data/train/segments data/train
+  cp data/train/vad.scp data/train
   utils/fix_data_dir.sh data/train
 fi
+echo "Stage 4: Remove silence frames done."
 
 if [ $stage -le 5 ]; then
   # Now, we need to remove features that are too short after removing silence
@@ -120,7 +183,7 @@ if [ $stage -le 5 ]; then
   utils/fix_data_dir.sh data/train
 fi
 
-echo "Stage 2-5: Remove too short utterances, speakers with few utterances done."
+echo "Stage 5: Remove too short utterances, speakers with few utterances done."
 
 # Stages 6 through 8 are handled in run_xvector.sh
 local/nnet3/xvector/run_xvector.sh --stage $stage --train-stage -1 \
@@ -131,7 +194,7 @@ echo "Stage 6-8: Train nnet done."
 
 if [ $stage -le 9 ]; then
   # Extract x-vectors for centering, LDA, and PLDA training.
-  sid/nnet3/xvector/extract_xvectors.sh --cmd "$train_cmd --mem 4G" --nj 80 \
+  sid/nnet3/xvector/extract_xvectors.sh --cmd "$train_cmd --mem 4G" --nj 80 --use-gpu true \
     $nnet_dir data/train \
     $nnet_dir/xvectors_train
 fi
@@ -158,4 +221,4 @@ if [ $stage -le 10 ]; then
     $nnet_dir/xvectors_train/plda || exit 1;
 fi
 
-echo "Stage 10: Train PLDA done."
+echo "Stage 10: Train PLDA done with LDA=$lda_dim"
